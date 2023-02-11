9e0271317fbc06b8b793706c16d081186aa1fda3
Abra <abdowaheb111@gmail.com>
2023-02-11 04:40:36 +0200

    commit

--- app/src/main/java/com/grad/dawinii/view/main/RoutineFragment.kt
+++ app/src/main/java/com/grad/dawinii/view/main/RoutineFragment.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
+import android.text.format.DateFormat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,10 +16,16 @@ import android.widget.EditText
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.grad.dawinii.R
 import com.grad.dawinii.adapter.RoutineRecyclerAdapter
+import com.grad.dawinii.databinding.AddAppointmentDialogBinding
+import com.grad.dawinii.databinding.AddMedicineDialogLayoutBinding
 import com.grad.dawinii.databinding.FragmentRoutineBinding
 import com.grad.dawinii.model.entities.Routine
+import com.grad.dawinii.util.makeToast
 import com.grad.dawinii.util.setupMonth
 import java.util.Calendar
 
@@ -35,8 +42,7 @@ class RoutineFragment : Fragment() {
     var appointmentDate = ""
     var appointmentTime = ""
     var doctorName = ""
-    val builder = AlertDialog.Builder(context)
-    val alertDialog = builder.show()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -65,12 +71,8 @@ class RoutineFragment : Fragment() {
         binding.fabAddRoutine.setOnClickListener {
             Navigation.findNavController(binding.fabAddRoutine).navigate(R.id.action_to_add_routine)
         }
-        binding.fabAddAppointment.setOnClickListener {
-            builder.setView(R.layout.add_appointment_dialog)
-            doctorName = alertDialog.findViewById<EditText>(R.id.doctor_name).text.toString()
-            alertDialog.findViewById<Button>(R.id.appointment_date).setOnClickListener(){
-                pickAppointmentDate()
-            }
+        binding.fabAddAppointment.setOnClickListener{
+            showAddAppointmentDialog()
         }
         binding.fabAdd.setOnClickListener {
             setVisibility(clicked)
@@ -79,15 +81,52 @@ class RoutineFragment : Fragment() {
         }
     }
 
-    private fun pickAppointmentDate() {
+    private fun showAddAppointmentDialog() {
+        val dialog = BottomSheetDialog(requireContext())
+        val dialogBinding= AddAppointmentDialogBinding.inflate(layoutInflater,null,false)
+        dialog.setContentView(dialogBinding.root)
+        doctorName = dialogBinding.etDoctorName.text.toString()
+        dialogBinding.appointmentDate.setOnClickListener {
+            pickAppointmentDate(dialogBinding)
+        }
+        dialogBinding.appointmentTime.setOnClickListener {
+            pickAppointmentTime(dialogBinding)
+        }
+        dialogBinding.btnSubmit.setOnClickListener {
+            makeToast(context,"Doctor name = $doctorName\nAppointment Date = $appointmentDate\nAppointment Time = $appointmentTime")
+            dialog.dismissWithAnimation = true
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    private fun pickAppointmentTime(dialogBinding: AddAppointmentDialogBinding) {
+        val isSystem24Hour = DateFormat.is24HourFormat(requireContext())
+        val timeFormat = if (isSystem24Hour) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H
+        val timePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(timeFormat)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Medicine Time")
+            .build()
+        timePicker.show(childFragmentManager, "TAG")
+        timePicker.addOnPositiveButtonClickListener {
+            val hour = timePicker.hour
+            val minute = timePicker.minute
+            appointmentTime = "$hour:$minute"
+            dialogBinding.appointmentTime.text = appointmentTime
+        }
+    }
+
+    private fun pickAppointmentDate(dialogBinding: AddAppointmentDialogBinding) {
         val calender :Calendar = Calendar.getInstance()
         val year = calender.get(Calendar.YEAR)
         val month = calender.get(Calendar.MONTH)
         val dayOfWeek = calender.get(Calendar.DAY_OF_MONTH)
-        val dialog = DatePickerDialog(context as Context,DatePickerDialog.OnDateSetListener{datePicker, year, month, day ->
+        val dialog = DatePickerDialog(context as Context,{datePicker, year, month, day ->
             appointmentDate =  "${setupMonth(month)} ${day.toString().padStart(2, '0')}, $year"
-            alertDialog.findViewById<Button>(R.id.appointment_date).text = appointmentDate
-        },year,month,dayOfWeek)
+            dialogBinding.appointmentDate.text = appointmentDate
+        },year,month,dayOfWeek).show()
 
     }
 
