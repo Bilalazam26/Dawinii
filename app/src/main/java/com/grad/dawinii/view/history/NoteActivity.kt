8f1b8d66ebc128fb1c5de66789ba99b904faeecb
Abra <abdowaheb111@gmail.com>
2023-04-24 18:04:04 +0200

    finish activity_note.xml

--- app/src/main/java/com/grad/dawinii/view/history/NoteActivity.kt
+++ app/src/main/java/com/grad/dawinii/view/history/NoteActivity.kt
@@ -2,6 +2,7 @@ package com.grad.dawinii.view.history
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.grad.dawinii.R
 import com.grad.dawinii.adapter.NoteRecyclerAdapter
 import com.grad.dawinii.databinding.ActivityNoteBinding
@@ -10,7 +11,6 @@ import com.grad.dawinii.model.entities.Note
 class NoteActivity : AppCompatActivity() {
     lateinit var binding: ActivityNoteBinding
     lateinit var adapter: NoteRecyclerAdapter
-    val notes = mutableListOf<Note>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNoteBinding.inflate(layoutInflater)
@@ -22,14 +22,33 @@ class NoteActivity : AppCompatActivity() {
         val titles = arrayOf("Note1", "Note 2")
         val bodies = arrayOf("The body of note 1", "The body of note 2")
         val dates = arrayOf("23/9", "1/8")
-        for (i in 0..1){
+        val notes = mutableListOf<Note>()
+        for (i in 0..1) {
             notes.add(Note(title = titles[i], body = bodies[i], date = dates[i]))
         }
+        adapter.addData(notes)
     }
 
     private fun initView() {
-        setupDataForRecycler()
+        setupRecycler()
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        supportActionBar?.apply {
+            title = resources.getString(R.string.doctor_s_note)
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    private fun setupRecycler() {
         adapter = NoteRecyclerAdapter()
         binding.noteRecycler.adapter = adapter
+        setupDataForRecycler()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }
